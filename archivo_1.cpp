1f16487bcf1eaf36e4f34db528aee997bca54b38
luisviafara <viafara.luis@correounivalle.edu.co>
2026-05-10 23:48:25 -0500

    completo_ archivo_1.cpp

--- archivo_1.cpp
+++ archivo_1.cpp
@@ -3,10 +3,12 @@ using namespace std;
 
 int main {}
 {
-char nombre;
+char nombre,materia;
 int codigo;
 cout << "LUIS DAVID VIAFARA ZAPATA"<< endl;
 cout << "2516488"<< endl;
+cout << "METOLOGIA DE DESARROLO DE SOFTWARE"<< endl;
 cin>> codigo;
 cin>> nombre;
+cin>> materia;
 };
